88ab2b28fece73c1c9f83add6d273b149c62d6cb
david <grozzlybear@hotmail.co.uk>
2022-01-17 16:44:28 +0000

    Initial commit

---
+++ src/test/java/hulld/redbridge/udemy/webapp01/Webapp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package hulld.redbridge.udemy.webapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
